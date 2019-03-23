75d2301737ba853829f016c65bfd71ff9182597f
Jacob Mosteller <jacobm@mostellerllc.com>
2019-03-23 09:27:44 -0600

    destructuring done

--- destructuring.js
+++ destructuring.js
@@ -22,7 +22,7 @@ var carDetails = {
 */
 
 //Code Here
-
+const {color,make,model,year}=carDetails;
 
 
 ////////// PROBLEM 2 //////////
@@ -35,7 +35,7 @@ var carDetails = {
 
 function greeting( obj ) {
   //Code Here
-  
+  const {title,firstName,lastName}=obj;
   // Do not edit the code below.
   return 'Hello, ' + title + ' ' + firstName + ' ' + lastName + '!';
   // Do not edit the code above.
@@ -54,7 +54,10 @@ function greeting( obj ) {
 */
 
 //Code Here
-
+function totalPopulation(obj){
+  const{utah,california,texas,arizona}=obj;
+  return utah+california+texas+arizona;
+}
 
 
 ////////// PROBLEM 4 //////////
@@ -69,7 +72,12 @@ function greeting( obj ) {
 
 //Code Here
 
-
+function ingredients(obj){
+  const{carb,fat,protein}=obj;
+  let arr=[];
+  arr.push(carb,fat,protein);
+  return arr;
+}
 
 ////////// PROBLEM 5 //////////
 
@@ -86,7 +94,26 @@ function greeting( obj ) {
 */
 
 //Code Here
-
+function largeNumbers({first,second,third}){
+  if(first>second){
+    if(second<third){
+      return second;
+    }
+    else{return third;}
+  }
+  else if(first<second){
+    if(first<third){
+      return first;
+    }
+    else{return third;}
+  }
+  if(second>third){
+    if(third<first){
+      return third
+    }
+    else{return first}
+  }
+}
 
 
 ////////// PROBLEM 6 //////////
@@ -98,5 +125,14 @@ function greeting( obj ) {
 */
 
 //Code Here
-
-
+function numberGroups({a,b,c}){
+ if(a.length>b.length&&a.length>c.length){
+   return a
+ }
+ else if(b.length>a.length&&b.length>c.length){
+   return b
+ }
+ else if(c.length>a.length&&c.length>b.length){
+   return c
+ }
+}
